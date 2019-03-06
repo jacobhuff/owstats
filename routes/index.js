f2a3fe9a4f3d80b22ef79dba1960e89f41f0dad7
Jacob Huff <jacobhuff1313@gmail.com>
2019-03-06 15:15:29 -0500

    initial upload

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const ow = require('owapi');
+
+module.exports = {
+    getHomePage: async (req, res) => {
+        const data = [];
+        
+        // collect data from database and render it on home page
+
+        //res.send(compData);
+        //res.render('index.ejs', {data: data});
+    }
+};
